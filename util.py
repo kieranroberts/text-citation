622c802b5ecc9bce89b5c2e7a4e76fec9cc92cc5
Kieran Roberts <kroberts012@gmail.com>
2021-10-17 11:56:45 +0100

    Added new condition on valid sentence

--- util.py
+++ util.py
@@ -1,12 +1,14 @@
 import re
 
 def str_to_sentences(x):
-    return x.split('.')
+    sentences = x.split('.')
+    sentences = list(map(lambda s : re.sub('^[\s]+', '', s).rstrip(), sentences))
+    return sentences
     
 def is_valid_sentence(sentence):
     sentence = re.sub('^[ ]+', '', sentence)
-    pattern = re.compile(r'^[A-Za-z0-9]')
-    if re.match(pattern, sentence):
+    pattern = re.compile(r'^[\'\"A-Za-z]')
+    if re.match(pattern, sentence) and len(sentence.lstrip().rstrip().split(' '))>2:
         return True
     else:
         return False
