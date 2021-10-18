ed7c0e148928239464aa0a1e21ec27ee759c9274
Kieran Roberts <kroberts012@gmail.com>
2021-10-18 21:07:59 +0100

    renamed methods

--- wiki_extractor.py
+++ wiki_extractor.py
@@ -5,11 +5,14 @@ import re
 from util import is_valid_sentence
 from wiki_utils import exclude_substring_sections
         
-def wiki_search(query, results=10):
-    return wikipedia.search(query=query, results=results)
+def wiki_query(query, results=1):
+    if len(query)<300:
+        return wikipedia.search(query=query, results=results)
+    else:
+        return None
 
 
-def page_extractor(page):
+def page_extractor(page, first_N_lines=10):
     if not(page.endswith('(disambiguation)')):
         try:
             response = wikipedia.page(page, auto_suggest=False)
@@ -17,12 +20,26 @@ def page_extractor(page):
                         'content' : response.content}
             all_sentences = res['content'].split('.')
             valid_sentences = list(filter(is_valid_sentence, all_sentences))
-            res['sentences'] = valid_sentences
+            valid_sentences
+            res['sentences'] = valid_sentences[:first_N_lines]
             return res
         except wikipedia.exceptions.DisambiguationError as e:
             print('Skipping disambgiuous pages: {}'.format(e.options))
             return False
-    
+
+def wiki_search(query):
+    entries = list()
+    pages = wiki_query(query, results=1)
+    if pages:
+        for page in pages:
+            res = page_extractor(page)
+            keys = list()
+            values = list()
+            for k,v in res.items():
+                keys.append(k)
+                values.append(v)
+            entries.append(dict(zip(keys,values)))
+    return entries
 
                 
     
