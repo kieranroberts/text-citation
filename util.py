f6cf26ff285411d90ce8b6a46e5bc41edc69c0f2
Kieran Roberts <kroberts012@gmail.com>
2021-10-27 19:54:48 +0100

    Changed definition of valid sentence

--- util.py
+++ util.py
@@ -2,16 +2,16 @@ import re
 
 def str_to_sentences(x):
     sentences = x.split('\n\n')
-    sentences = [re.split('\!|\.',x) for x in sentences]
+    sentences = [re.split('\.(?![0-9A-Za-z])', x) for x in sentences]
     sentences = [item for sent in sentences for item in sent if item != ' ']
     sentences = [re.sub('\n', '', sent) for sent in sentences]
-    #sentences = list(map(lambda s : re.sub('^[\s]+', '', s).rstrip(), sentences))
+    sentences = list(map(lambda s : re.sub('^[\s]+', '', s), sentences))
     return sentences
     
 def is_valid_sentence(sentence):
     sentence = re.sub('^[ ]+', '', sentence)
     pattern = re.compile(r'^[\'\"A-Za-z]')
-    if re.match(pattern, sentence) and len(sentence.lstrip().rstrip().split(' '))>2:
+    if re.match(pattern, sentence) and len(sentence.lstrip().rstrip().split(' '))>3:
         return True
     else:
         return False
