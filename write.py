0ff9bdc1ea5b1446623190bed9e8fc1164eb7cf0
Kieran Roberts <kroberts012@gmail.com>
2021-10-27 14:39:54 +0100

    Added method to clean text

--- write.py
+++ write.py
@@ -1,13 +1,15 @@
 from extract import ExtractPage
 import os
+import re
 
 class ExtractBook:
     
-    def __init__(self, page_ordering, home_dir, num_pages):
+    def __init__(self, page_ordering, home_dir, num_pages, title):
         self.page_ordering = page_ordering # dictionary {1 : 'page_1.png', etc.}
         self.home_dir = home_dir
         self.num_pages = num_pages
         self.full_book = None
+        self.title = title
     
     def convert_book_to_text(self):
         total_str = str()
@@ -17,6 +19,17 @@ class ExtractBook:
             total_str +=  page_extractor.extract_text()
         
         self.full_book = total_str
+    
+    def clean_book_text(self):
+        self.full_book = re.sub(r"([0-9]+\.)([A-Za-z]+)", r"\g<1> \g<2>", self.full_book)
+        self.full_book = re.sub(r"([A-Za-z]+\.)([A-Za-z]+)", r"\g<1> \g<2>", self.full_book)
+        self.full_book = re.sub(r"([0-9]+\.)([0-9]{1}[A-Za-z]+)", r"\g<1> \g<2>", self.full_book)
+        self.full_book = re.sub(r"\x0c", "", self.full_book)
+        self.full_book = re.sub(self.title, "", self.full_book)
+        self.full_book = self.title + "\n\n" + self.full_book
+    
+    def reorder_contents_page(self):
+        pass
     
     def write_full_book_to_file(self, path_to_output):
         with open(path_to_output, 'w') as txt:
