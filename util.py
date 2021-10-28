ef8b4031a57ce00705040348001d62652872df38
Kieran Roberts <kroberts012@gmail.com>
2021-10-28 10:16:47 +0100

    improved sentence extractor;added section detector

--- util.py
+++ util.py
@@ -4,8 +4,9 @@ def str_to_sentences(x):
     sentences = x.split('\n\n')
     sentences = [re.split('\.(?![0-9A-Za-z])', x) for x in sentences]
     sentences = [item for sent in sentences for item in sent if item != ' ']
-    sentences = [re.sub('\n', '', sent) for sent in sentences]
-    sentences = list(map(lambda s : re.sub('^[\s]+', '', s), sentences))
+    sentences = [re.sub('\n', ' ', sent) for sent in sentences]
+    sentences = [sent.rstrip().lstrip() for sent in sentences]
+    #sentences = list(map(lambda s : re.sub('^[\s]+', '', s), sentences))
     return sentences
     
 def is_valid_sentence(sentence):
@@ -15,3 +16,9 @@ def is_valid_sentence(sentence):
         return True
     else:
         return False
+
+def is_section_name(last_sentence, sentence):
+    if len(sentence.split(' ')) <= 3 and sentence.split(' ') != [''] and last_sentence == '':
+            return True
+    else:
+            return False
