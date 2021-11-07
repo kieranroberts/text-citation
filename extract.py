fd8dd73cec039c9a5fc4961618c4b1a7eaac93c4
Kieran Roberts <kroberts012@gmail.com>
2021-11-07 19:07:25 +0000

    Change class name, tidy up

--- extract.py
+++ extract.py
@@ -1,23 +1,9 @@
 from PIL import Image
 import pytesseract
-#import wikipedia
-from typing import List
-
-
-pytesseract.pytesseract.tesseract_cmd = 'c:\\Users\\asunder\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
-
-class ExtractPage:
-    
-    def __init__(self, path_to_image):
-        self.path_to_image = path_to_image
-    
-    def extract_text(self):
-        return pytesseract.image_to_string(Image.open(self.path_to_image))
-
-
 class Page:
-    def __init__(self, *, image_path: str):
+    def __init__(self, *, image_path: str, pytesseract_cmd: str):
         self.image_path = image_path
+        pytesseract.pytesseract.tesseract_cmd = self.pystessreact
         self.text = pytesseract.image_to_string(Image.open(self.image_path))
 
     @property
